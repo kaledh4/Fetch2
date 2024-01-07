a821a99e6d53b1657320b73b2f202624f6627135
Medo <kaled.h4@gmail.com>
2024-01-07 11:16:59 +0300

    Update Values.js

--- java/Values.js
+++ java/Values.js
@@ -1,7 +1,7 @@
 const sheetId = '1IV_roEtzjbfVRh9YaLfW0fBZmNZjdZ_KRYiPHwMXCj4';
 const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
 const sheetName = 'Values';
-const query = encodeURIComponent('Select A, B, C, D, E, F, G, H, I, J, K') // Changed from 'Select *'
+const query = encodeURIComponent('Select A, B, C, D, E, F, G, H, I, J, K limit 18');
 const url = `${base}&sheet=${sheetName}&tq=${query}`
 const data = []
 document.addEventListener('DOMContentLoaded', init)
